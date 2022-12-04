c25498e6dd612e1297e4780ec1a96171b4a3c88f
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-12-04 16:54:30 -0300

    DB probably finished

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,10 +3,42 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define("recipe", {
+    id: {
+      // UUID: Un ID propio de Sequelize conformado por letras y números.
+      type: DataTypes.UUID,
+      defaultType: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    dishSummary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    instructions: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+
+    //Adicional traemos la imagen.
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    // Si queremos traer algo de la base de datos lo distinguimos así.
+    createdInDB: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };
